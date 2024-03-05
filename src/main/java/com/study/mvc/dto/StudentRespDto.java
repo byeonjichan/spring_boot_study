3ab1d87c92c7d18dc1340a29294282718ee8e088
Byeonjichan <jaekyeon14@naver.com>
2024-03-05 17:56:36 +0900

    :03.05

--- src/main/java/com/study/mvc/dto/StudentRespDto.java
+++ src/main/java/com/study/mvc/dto/StudentRespDto.java
@@ -5,12 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 @Builder
 public class StudentRespDto {
     private String name;
     private int age;
     private String phone;
     private String address;
+
+    public StudentRespDto toRespDto() {
+        return StudentRespDto.builder()
+                .name(name)
+                .age(age)
+                .phone(phone)
+                .address(address)
+                .build();
+    }
 }
 
